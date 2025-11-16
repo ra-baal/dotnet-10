07cd308870433cf544903fe95647efc40eb16c77
ra-baal <balcerowskirafal@gmail.com>
2025-11-16 20:36:31 +0100

    User defined compound assignment

---
+++ CSharp14/ChangesOverview/UserDefinedCompoundAssignment.cs
@@ -0,0 +1,41 @@
+﻿namespace CSharp14.ChangesOverview;
+
+internal class UserDefinedCompoundAssignment
+{
+    internal static void Demo()
+    {
+        Counter counter = new();
+        Counter c = counter++; // A - wywoluje statyczne ++
+        counter++; // B - wywoluje instancyjne ++ 
+        counter += 5; // C - wywoluje instancyjne += 
+        Counter c2 = counter += 5; // C - wywoluje instancyjne += i zwraca obiekt (sic!)
+    }
+}
+
+class Counter
+{
+    public int Value;
+
+    // A //
+    // C# 13 - operator ++ jako metoda statyczna zwracajaca obiekt.
+    public static Counter operator ++(Counter counter)
+    {
+        counter.Value = counter.Value + 1;
+        return counter;
+    }
+
+    // B //
+    // C# 14 - operator ++ jako metoda obiektu zwracajaca void.
+    public void operator ++()
+    {
+        Value++;
+    }
+
+    // C //
+    // C# 14 - mozliwosc samodzielnego zdefiniowania operatora +=
+    // - metoda obiektu zwracajaca void
+    public void operator +=(int x)
+    {
+        Value += x;
+    }
+}
--- CSharp14/Program.cs
+++ CSharp14/Program.cs
@@ -3,19 +3,27 @@
 Console.WriteLine("Nowości w C# 14\n");
 
 // #1 The field keyword
+// Slowo kluczowe field
 
 // #2 Extension members
+// Rozszerzajace elementy czlonkowskie
 //ExtensionMembers.Demo();
 
 // #3 Implicit span conversions
+// Niejawne konwersje span
 
 // #4 Unbound generic types and nameof
+// Nieskonkretyzowane typy generyczne i nameof
 //UnboundGenericTypesAndNameof.Demo();
 
 // #5 Simple lambda parameters with modifiers
+// Proste parametry lambda z modyfikatorami
 
 // #6 More partial members
+// Wiecej elementow czesciowych
 
 // #7 User defined compound assignment
+// Przypisanie zlozone zdefiniowane przez uzytkownika
 
 // #8 Null-conditional assignment
+// Przypisanie warunkowe wzgledem null
